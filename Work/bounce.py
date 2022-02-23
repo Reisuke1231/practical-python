6c706a73f050aa2081e7cefd8a467961f88bf8b3
Reisuke Hashimoto <reisuke-hashimoto@reiscreation.com>
2022-02-23 12:53:40 +0900

    Write bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+count = 0
+DROP_HEIGHT = 100
+
+bounce_height = DROP_HEIGHT
+while count < 10:
+    bounce_height = bounce_height * (3/5)
+    print(f'{count+1}: {round(bounce_height, 4)}')
+
+    count += 1
